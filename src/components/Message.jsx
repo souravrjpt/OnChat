6ff599974a7e04dcff285a0969520bbfd16ca394
souravrjpt <souravrajput2760@gmail.com>
2024-02-25 22:29:47 +0530

    25/02/2024 edited the message section and added scss

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React from 'react';
+import img from '../images/file_profile.png';
 
-const Message = ()=>{
-    return(
-        <div >Message</div>
-    )
-}
-export default Message
+const Message = () => {
+  return (
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img src={img} alt='' />
+        <span>just now</span>
+      </div>
+        <div className='messageContent'>
+          <p>hello hi my name is sourav</p>
+          <img src={img} alt='' />
+        </div>
+    </div>
+  );
+};
+export default Message;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' placeholder='find a user' />
+        <input type='text' placeholder='Find a user' />
       </div>
       <div className='userChat'>
         <img src='https://media.istockphoto.com/id/1170154554/photo/young-handsome-business-man-dressed-in-casual-denim-shirt-with-smartwatch-on-wrist-isolated.jpg?s=612x612&w=0&k=20&c=DOZVh06WmLM7Pcuh07Glxtr54d-LfMZX4dxVGhVTELk=' />
