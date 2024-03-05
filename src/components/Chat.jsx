0c2d50159c7fd32325723d4769bf71c658eaa3fe
souravrjpt <souravrajput2760@gmail.com>
2024-03-06 02:38:12 +0530

    06/03/2024 added functionality for sending text and images

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Add from '../images/add.png';
 import Cam from '../images/camera.png';
 import Vid from '../images/video.png';
 import Messages from './Messages';
 import Input from './Input';
+import { ChatContext } from '../context/ChatContext';
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
   return (
     <div className='chat'>
       <div className='chatInfo'>
-        <span>Sourav</span>
+        <span>{data.user?.displayName}</span>
         <div className='chatIcons'>
           <img src={Add} alt='' />
           <img src={Cam} alt='' />
@@ -18,8 +20,6 @@ const Chat = () => {
       </div>
       <Messages />
       <Input />
-
-      
     </div>
   );
 };
