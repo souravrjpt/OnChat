c0400647a444e88de08f2b4570cb40b72e0fe898
souravrjpt <souravrajput2760@gmail.com>
2024-03-09 20:13:12 +0530

    09/03/2024 edited some mistakes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const Login = () => {
   const [err, setErr] = useState(false);
@@ -14,23 +14,25 @@ const Login = () => {
 
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate("/")
+      navigate('/');
     } catch (err) {
       setErr(true);
     }
   };
   return (
-    <div className="formContainer">
-      <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
-        <span className="title">Login</span>
+    <div className='formContainer'>
+      <div className='formWrapper'>
+        <span className='logo'>OnChat</span>
+        <span className='title'>Login</span>
         <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
+          <input type='email' placeholder='email' />
+          <input type='password' placeholder='password' />
           <button>Sign in</button>
           {err && <span>Something went wrong</span>}
         </form>
-        <p>You don't have an account? <Link to="/register">Register</Link></p>
+        <p>
+          You don't have an account? <Link to='/register'>Register</Link>
+        </p>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -62,7 +62,7 @@ const Register = () => {
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
-        <span className='logo'>ONCHAT</span>
+        <span className='logo'>OnChat</span>
         <span className='title'>Register</span>
         <form onSubmit={handleSubmit}>
           <input type='text' placeholder='display name' />
